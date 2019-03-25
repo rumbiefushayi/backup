d80e81039d3f4cee16885d060aa1e1ffc5c33978
Rumbie <rumbiefushayie@gmail.com>
2019-03-25 11:55:51 +0200

    git terms

---
+++ MEAN/index.js
@@ -0,0 +1,11 @@
+//using node to give an ouput in the command line
+var one = 1
+var two = 2
+var three = one * two
+console.log(three);
+//
+var four = [1,2,3,4,5,6,7,8,9,10]
+for(i=0; i<four.length; i++) {console.log("The index is " + i + "the value is " + four[i]);
+}
+//
+console.log("Hi! This is my first server");
